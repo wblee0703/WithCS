35ee51480933d3a60bae9d9816633f1fb66b37ed
leewonbae <lwb0703@gmail.com>
2026-02-19 10:37:52 +0900

    로그인 정보 자동입력 삭제

--- static/Setting DAO/index.js
+++ static/Setting DAO/index.js
@@ -980,7 +980,6 @@ function setupCalendar() {
         popup.onclick = (e) => { if (e.target === popup) popup.style.display = 'none'; };
     }
     if (searchInput) {
-        searchInput.setAttribute('autocomplete', 'off');
         searchInput.addEventListener('input', () => {
             renderCalendar();
         });
@@ -2190,7 +2189,6 @@ function setupGanttSearch() {
     const filterBtn = document.getElementById('btn-gantt-filter');
 
     if (searchInput) {
-        searchInput.setAttribute('autocomplete', 'off');
         searchInput.addEventListener('input', () => {
             renderGanttChart();
         });
