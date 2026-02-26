e116b9b6c69d8f07cb33c359cfaee9a91793090f
leewonbae <lwb0703@gmail.com>
2026-02-26 12:58:08 +0900

    Auto-save: 2026-02-26 12:58:08

--- SettingDAO/JavaScript/calendar.js
+++ SettingDAO/JavaScript/calendar.js
@@ -276,7 +276,7 @@ function renderMonthGrid(year, month, titleId, gridId) {
                     dragAttr = `draggable="true" data-drag-site="${escapeHtml(group.site)}" data-drag-equip="${escapeHtml(group.equip)}" data-drag-ids="${idsJson}" ondragstart="handleCalendarDragStartFromData(event)" ondragend="this.classList.remove('dragging')"`;
                 }
 
-                eventsHtml += `<div class="calendar-event-item ${completedClass}" ${dragAttr} onclick="event.stopPropagation(); openCalendarPopup('${dateStr}', dayEvents)">
+                eventsHtml += `<div class="calendar-event-item ${completedClass} ${typeClass}" ${dragAttr} onclick="event.stopPropagation(); openCalendarPopup('${dateStr}', dayEvents)">
                     ${escapeHtml(group.site)} ${escapeHtml(equipName)} <span class="event-type-text ${typeClass}">${group.type}</span>
                 </div>`;
             });
