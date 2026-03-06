37c25e82b46fca85d122bd450772ea0f605b9b1f
leewonbae <lwb0703@gmail.com>
2026-03-06 11:01:49 +0900

    Auto-save: 2026-03-06 11:01:49

--- SettingDAO/JavaScript/gantt.js
+++ SettingDAO/JavaScript/gantt.js
@@ -31,6 +31,7 @@ let currentExecStartTargetId = null;
    ========================================================================== */
 document.addEventListener('DOMContentLoaded', () => {
     // 간트 차트 관련 초기화
+    setupGanttSearch();
     setupGanttZoom();
     setupGanttResizer();
     setupEquipInfoResizer();
@@ -42,6 +43,14 @@ document.addEventListener('DOMContentLoaded', () => {
         execEndDate.addEventListener('change', checkExecDelayStatus);
     }
 });
+
+function setupGanttSearch() {
+    const filterBtn = document.getElementById('btn-gantt-filter');
+
+    if (filterBtn) {
+        filterBtn.onclick = () => openGanttSearchModal();
+    }
+}
 
 function setupGanttZoom() {
     const btnExpand = document.getElementById('btn-gantt-expand');
