d3eabbd6e50691d99757d11a7384381b33e79974
leewonbae <lwb0703@gmail.com>
2026-03-18 08:28:48 +0900

    Auto-save: 2026-03-18 08:28:47

--- SettingDAO/JavaScript/maintenance.js
+++ SettingDAO/JavaScript/maintenance.js
@@ -1673,10 +1673,13 @@ function handleFiles(files) {
     const key = `details_${currentPath.site}_${currentPath.equip}`;
     // 용량 체크 (간단히 2MB 제한)
     const MAX_SIZE = 2 * 1024 * 1024; 
+    // 용량 체크 (100KB 제한)
+    const MAX_SIZE = 100 * 1024; 
 
     Array.from(files).forEach(file => {
         if (file.size > MAX_SIZE) {
             alert(`파일 '${file.name}'이 너무 큽니다. (2MB 제한)`);
+            alert(`파일 '${file.name}'이 너무 큽니다. (100KB 제한)`);
             return;
         }
 
