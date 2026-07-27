f11f717f91490407c9c6063835e96c9f6cf9c212
leewonbae <lwb0703@gmail.com>
2026-07-27 19:50:27 +0900

    수정

--- app.py
+++ app.py
@@ -802,11 +802,11 @@ def login():
     user = User.query.filter_by(id=user_id).first()
 
     
-    # 전체 로그인 차단 (정비 중) - 최종관리자는 허용
-    if user and user.role in ['superadmin']:
-        pass  # allow login to proceed
-    else:
-        return jsonify({"status": "fail", "message": "서버 점검 중으로 현재 접속 불가능 합니다. 죄송합니다."}), 403
+    # Login block removed for normal operation
+    # if user and user.role in ['superadmin']:
+    #     pass  # allow login to proceed
+    # else:
+    #     return jsonify({"status": "fail", "message": "서버 점검 중으로 현재 접속 불가능 합니다. 죄송합니다."}), 403
 
     # 1. 계정 잠금 확인
     now = datetime.now()
